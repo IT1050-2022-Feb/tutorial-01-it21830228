775df38d2c61f5e6e36f9791fc7efbb33fde2c7e
it21830228 <111220362+it21830228@users.noreply.github.com>
2022-08-14 17:48:53 +0530

    Tute 1

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+   int no1 , no2;
+   float average;
+   
+   printf("enter the first number :");
+   scanf("%d",&no1);
+   printf("enter the second number :");
+   scanf("%d",&no2);
+   
+   average = (no1 + no2)/2
+      
+   printf("average of the two marks :");
+   
   
   return 0;
 }
